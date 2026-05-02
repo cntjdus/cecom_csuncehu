223c19b98eab6a39c8ab6048c1895f1b1253eef8
cntjdus <smonstacy@cau.ac.kr>
2026-05-03 02:25:51 +0900

    세콤짝선짝후웹페이지 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import devServer from '@hono/vite-dev-server'
 import pages from '@hono/vite-cloudflare-pages'
 
 export default defineConfig({
-  plugins: [pages()],
+  plugins: [
+    devServer({
+      entry: 'src/index.tsx'
+    }),
+    pages()
+  ],
   build: {
     outDir: 'dist'
   }
